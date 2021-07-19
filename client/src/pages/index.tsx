26d3c95725cf68a3db54898df80f265868f3e550
HongJunHyeok <edb1631@naver.com>
2021-07-19 16:49:00 +0900

    ADD: @emotion package installing

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const StyledBox = styled.div`
+  padding: 100px;
+  background-color: black;
+`;
 
 const MainPage = () => {
-  return <>Code Bank</>;
+  return (
+    <>
+      Code Bank
+      <StyledBox />
+    </>
+  );
 };
-
 export default MainPage;
