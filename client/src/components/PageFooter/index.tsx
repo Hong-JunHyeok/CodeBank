417814e57a8d7a688ce37a8d96ae72872fa787d4
HongJunHyeok <edb1631@naver.com>
2021-07-20 01:31:26 +0900

    ADD: Footer styling

--- client/src/components/PageFooter/index.tsx
+++ client/src/components/PageFooter/index.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import { FooterContainer } from "./styles";
+import CodeBankLogo from "../../../public/codebank_logo_white.png";
 
 const PageFooter = () => {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <div className="logo-container">
+        <Image src={CodeBankLogo} width={100} height={100} />
+      </div>
+      <div className="copywrite-container">
+        Copyright ⓒ 2021~ CodeBank. All rights reserved.
+      </div>
+    </FooterContainer>
+  );
 };
 
 export default PageFooter;
--- client/src/components/PageFooter/styles.tsx
+++ client/src/components/PageFooter/styles.tsx
@@ -1,7 +1,17 @@
 import styled from "@emotion/styled";
 
 export const FooterContainer = styled.footer`
-  padding: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   background-color: #262626;
   color: white;
+  .logo-container {
+    margin-top: 60px;
+  }
+  .copywrite-container {
+    margin-bottom: 50px;
+    font-weight: bold;
+  }
 `;
