ef75576f145d9332faef6b71595e68a5142c1481
HongJunHyeok <edb1631@naver.com>
2021-07-31 17:43:26 +0900

    ADD: Post routing delete & create

--- server/src/routes/post/index.ts
+++ server/src/routes/post/index.ts
@@ -20,4 +20,23 @@ router.get("/:post_id", (req, res) => {
   });
 });
 
+router.post("/", async (req, res) => {
+  const { title, content, field } = req.body;
+  return res.status(201).json({
+    message: "정상적으로 생성되었습니다.",
+    data: {
+      title,
+      content,
+      field,
+    },
+  });
+});
+
+router.delete("/:post_id", async (req, res) => {
+  const { post_id } = req.params;
+  return res.json({
+    message: `포스트 ${post_id}가 정상적으로 삭제되었습니다.`,
+  });
+});
+
 export default router;
